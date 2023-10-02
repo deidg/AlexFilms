8633271d2876b3d175cd8de181e344be93f88d26
deidg <64797316+deidg@users.noreply.github.com>
2023-10-02 09:13:37 +0300

    Initial Commit

---
+++ AlexFilms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlexFilms
+//
+//  Created by Alex on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
