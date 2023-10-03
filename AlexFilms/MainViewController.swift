b38e6c452041f8d6ed2aadd9a8851353f05864e8
deidg <64797316+deidg@users.noreply.github.com>
2023-10-03 12:53:02 +0300

    commit 1.0.2 Date:  03 oct 23
    Items inited

--- AlexFilms/MainViewController.swift
+++ AlexFilms/MainViewController.swift
@@ -1,21 +1,62 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  AlexFilms
 //
 //  Created by Alex on 02.10.2023.
 //
 
-import UIKit
+import Foundation
 import SnapKit
 
-class MainViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
+    private let mainLabelAuthPage: UILabel = {
+        let label = UILabel()
+        label.text = "Sign In"
+        return label
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter email"
+        return textfield
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter password"
+        return textfield
+    }()
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+        return button
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemRed
+//        view.backgroundColor = .white
+        
+        
     }
-
-
+    
+    private func setupUI() {
+        view.addSubview(mainLabelAuthPage)
+        mainLabelAuthPage.snp.makeConstraints { make in
+            make.top.equalTo(view).inset(250)
+            make.center.equalToSuperview()
+        }
+        
+    }
+    
+    
+    
+    
 }
-
