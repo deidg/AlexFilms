d087a9fa28b9418b736a41eafaa6cff7b0e057fd
deidg <64797316+deidg@users.noreply.github.com>
2023-10-22 19:05:59 +0300

    commit 1.2.5 Date:  22 oct 23
    TabBarController_Favourites done MVP (w/o logic)

--- AlexFilms/Controllers/TabBarControllerFavourites.swift
+++ AlexFilms/Controllers/TabBarControllerFavourites.swift
@@ -8,23 +8,67 @@
 import UIKit
 
 class TabBarControllerFavourites: UIViewController {
-
+    
+    private let headLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Favourites"
+        label.font = UIFont(name: "Ubuntu", size: 24)
+        return label
+    }()
+    
+    private let filmsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .cyan
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
+        
         view.backgroundColor = .white
-
+        
+        //        filmsTableView.delegate = self
+        filmsTableView.dataSource = self
+        filmsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        
+        view.addSubview(headLabel)
+        headLabel.snp.makeConstraints { make in
+            make.top.equalTo(view).inset(60)
+            make.centerX.equalToSuperview()
+        }
+        
+        view.addSubview(filmsTableView)
+        filmsTableView.snp.makeConstraints { make in
+            make.top.equalTo(headLabel.snp.bottom)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
-    */
-
+    
 }
+
+//extension TabBarControllerMain: UITableViewDelegate {
+//
+//    func filmsTableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("hello from cell")
+//    }
+//}
+
+extension TabBarControllerFavourites: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
+        cell.textLabel?.text = "favorite film"
+        return cell
+    }
+}
+
--- AlexFilms/Controllers/TabBarControllerMain.swift
+++ AlexFilms/Controllers/TabBarControllerMain.swift
@@ -104,12 +104,12 @@ class TabBarControllerMain: UIViewController {
 extension TabBarControllerMain: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return 5
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
-        cell.textLabel?.text = "poherr"
+        cell.textLabel?.text = "film name"
         return cell
     }
 }
